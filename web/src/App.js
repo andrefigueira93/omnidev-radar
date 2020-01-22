7b1e3658c504c36de577020552285d838c056537
andrefigueira93 <andrefigueira93@gmail.com>
2020-01-22 18:34:40 -0300

    Estilização da Sidebar

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,46 @@
+// Packages
 import React from 'react';
-import './styles/App.css';
+
+// Styles
 import './styles/global.css';
+import './styles/App.css';
+import './styles/sidebar.css';
 
 function App() {
-  return (
-    <div id="app">
-      <aside>
-      </aside>
-      <main>
-      </main>
-    </div>
-  );
+    return (
+        <div id="app">
+            <aside>
+                <strong>Cadastrar</strong>
+                <form>
+                    <div className="input-block">
+                        <label htmlFor="github_username">Usuário</label>
+                        <input name="github_username" id="github_username"/>
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="techs">Tecnologias</label>
+                        <input name="techs" id="techs"/>
+                    </div>
+
+                    <div className="input-group">
+                        <div className="input-block">
+                            <label htmlFor="latitude">Latitude</label>
+                            <input name="latitude" id="latitude"/>
+                        </div>
+                        <div className="input-block">
+                            <label htmlFor="longitude">Longitude</label>
+                            <input name="longitude" id="longitude"/>
+                        </div>
+                    </div>
+                    <button type="submit">
+                        Salvar
+                    </button>
+                </form>
+            </aside>
+            <main>
+            </main>
+        </div>
+    );
 }
 
 export default App;
